7b265e50248f0d5a1b5f86d1ae66d65c384561c3
Jeff Lewis <mail@jeffreylewis.me>
2025-01-20 20:23:52 -0500

    initial

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "arzg/vim-colors-xcode",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- load the colorscheme here
+      -- vim.cmd([[colorscheme tokyonight]])
+      vim.cmd("colorscheme " .. EcoVim.colorscheme)
+      require("config.colorscheme")
+    end,
+  },
+
+  {
+  "f-person/auto-dark-mode.nvim",
+  opts = {
+    update_interval = 1000,
+    set_dark_mode = function()
+      vim.api.nvim_set_option_value("background", "dark", {})
+      vim.cmd("colorscheme xcodedark")
+    end,
+    set_light_mode = function()
+      vim.api.nvim_set_option_value("background", "light", {})
+      vim.cmd("colorscheme xcodelight")
+    end,
+  },
+}
+}
